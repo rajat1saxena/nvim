8e0c8811e565c060c3b53b036f48d4c70af0c25d
Rajat Saxena <hi@rajatsaxena.dev>
2023-04-17 22:29:05 +0530

    nvimtree shortcut

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -5,8 +5,9 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 -- vim.opt.termguicolors = true
 
-vim.keymap.set('n', '<leader>nf', vim.cmd.NvimTreeFocus)  
-vim.keymap.set('n', '<leader>nt', vim.cmd.NvimTreeToggle)  
+vim.keymap.set('n', '<leader>nf', vim.cmd.NvimTreeFocus)
+vim.keymap.set('n', '<leader>nt', vim.cmd.NvimTreeToggle)
+vim.keymap.set('n', '<leader>no', vim.cmd.NvimTreeFindFile)
 
 -- empty setup using defaults
 require("nvim-tree").setup()
