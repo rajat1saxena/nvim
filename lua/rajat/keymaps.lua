7d8f9966bca2d473a148599c7001c2a2c872feb8
Rajat Saxena <hi@rajatsaxena.dev>
2023-04-20 01:27:55 +0530

    Keymaps for tabs

--- lua/rajat/keymaps.lua
+++ lua/rajat/keymaps.lua
@@ -8,3 +8,7 @@ vim.keymap.set('n', '<leader>rk', ':resize -10<CR>', opts)
 vim.keymap.set('n', '<leader>rj', ':resize +10<CR>', opts)
 vim.keymap.set('n', '<leader>rh', ':vertical resize -10<CR>', opts)
 vim.keymap.set('n', '<leader>rl', ':vertical resize +10<CR>', opts)
+
+-- tabs
+vim.keymap.set('n', '<S-h>', ':tabp<CR>') 
+vim.keymap.set('n', '<S-l>', ':tabn<CR>') 
